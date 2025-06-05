7940ac8fa7fc126c935bc3f030f7f0878d9f0e0d
Nikolerojas10 <nikolerojas10.nr30@gmail.com>
2025-06-05 15:20:12 -0500

    creacion proyecto ParImpar

---
+++ NúmeroParImpar/Program.cs
@@ -0,0 +1,18 @@
+﻿do
+{
+    Console.Write("Ingrese un numero:");
+    var numero = int.Parse(Console.ReadLine());
+
+    if (numero % 2 = 0)
+    {
+        Console.Write("El numero es Par");
+    }
+    else
+    {
+        Console.Write("El numero es impar");
+
+    } while (True)
+
+
+
+}
