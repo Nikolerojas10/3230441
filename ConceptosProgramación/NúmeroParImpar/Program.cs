efde22c11c27e91e56ec781ec9ddc48e59884418
Nikolerojas10 <nikolerojas10.nr30@gmail.com>
2025-06-05 15:57:17 -0500

    Proyeecto ParImpar

---
+++ ConceptosProgramación/NúmeroParImpar/Program.cs
@@ -0,0 +1,13 @@
+﻿do
+{
+    Console.Write("Ingrese un numero:");
+    int numero = int.Parse(s: Console.ReadLine());
+{
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine("El numero es Impar");
+    }
+    else
+    {
+        Console.WriteLine("El numero es Par");
+    }
